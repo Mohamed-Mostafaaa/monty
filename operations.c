7d6832ce3f40c9f4b1f44a65e8b61ee22830591e
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-11-17 03:57:28 +0200

    operations

---
+++ operations.c
@@ -0,0 +1,109 @@
+#include "monty.h"
+
+/**
+ * op_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+
+void op_add(stack_t **head, unsigned int c)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * op_sub - subtracts the top element of the stack
+ * from the second top element of the stack
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+void op_sub(stack_t **head, unsigned int c)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n - h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * op_div - divides the top element of the stack
+ * by the second top element of the stack
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+
+void op_div(stack_t **head, unsigned int c)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
