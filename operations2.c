2c73b7475397f4391886ecce051a3ecbef544a05
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-11-17 03:57:35 +0200

    operations

---
+++ operations2.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+
+/**
+ * op_mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+
+void op_mul(stack_t **head, unsigned int c)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * op_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+
+void op_mod(stack_t **head, unsigned int c)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
